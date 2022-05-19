7440eb1bba1d9bfb241b4e81bff3a5b6b6725914
Allison Henderson <allison.henderson@oracle.com>
2022-05-19 15:38:07 -0700

    xfsprogs: Add log attribute error tag
    Source kernel commit: abd61ca3c333506ffa4ee73b78659ab57e7efcf7
    
    This patch adds an error tag that we can use to test log attribute
    recovery and replay
    
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Chandan Babu R <chandanrlinux@gmail.com>
    Signed-off-by: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>

--- libxfs/defer_item.c
+++ libxfs/defer_item.c
@@ -173,6 +173,11 @@ xfs_attr_finish_item(
 	 */
 	args->trans = tp;
 
+	if (XFS_TEST_ERROR(false, args->dp->i_mount, XFS_ERRTAG_LARP)) {
+		error = -EIO;
+		goto out;
+	}
+
 	switch (op) {
 	case XFS_ATTR_OP_FLAGS_SET:
 		error = xfs_attr_set_iter(dac);
@@ -185,7 +190,7 @@ xfs_attr_finish_item(
 		error = -EFSCORRUPTED;
 		break;
 	}
-
+out:
 	if (error != -EAGAIN)
 		kmem_free(attr);
 
