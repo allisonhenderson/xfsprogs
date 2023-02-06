189eae9d0580402b6cc8c985a44e4b5f92e6adab
Allison Henderson <allison.henderson@oracle.com>
2023-02-05 23:11:42 -0700

    xfsprogs: remove parent pointers in unlink
    Source kernel commit: b9ffc3d05531820aea30b2caf3368c312d8b2508
    
    This patch removes the parent pointer attribute during unlink
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>

--- libxfs/xfs_attr.h
+++ libxfs/xfs_attr.h
@@ -545,6 +545,7 @@ bool xfs_attr_is_leaf(struct xfs_inode *ip);
 int xfs_attr_get_ilocked(struct xfs_da_args *args);
 int xfs_attr_get(struct xfs_da_args *args);
 int xfs_attr_defer_add(struct xfs_da_args *args);
+int xfs_attr_defer_remove(struct xfs_da_args *args);
 int xfs_attr_set(struct xfs_da_args *args);
 int xfs_attr_set_iter(struct xfs_attr_intent *attr);
 int xfs_attr_remove_iter(struct xfs_attr_intent *attr);
